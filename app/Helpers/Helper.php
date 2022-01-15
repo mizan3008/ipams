d6bd916925d8e8abe168ff0aeb4738be765b630c
Mizanur Rahman <mizan3008@gmail.com>
2022-01-15 08:23:06 +0600

    Add helper

---
+++ app/Helpers/Helper.php
@@ -0,0 +1,17 @@
+<?php
+
+function getClientIp(): string
+{
+    if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+        //from share internet
+        $ip = $_SERVER['HTTP_CLIENT_IP'];
+    } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+        //from proxy
+        $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    } else {
+        //from remote address
+        $ip = $_SERVER['REMOTE_ADDR'];
+    }
+
+    return $ip;
+}
